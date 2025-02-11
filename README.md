4b953250c13ed2480ab22762ee35e0277bcf9196
Vanessa Pihlaja <vanessa.pihlaja@aalto.fi>
2025-02-11 11:14:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# art_flutter_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/art_flutter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.art_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
